ac8f3e887075c54f717ff8383931e120c66b2203
kaustubh <saikaustubh10@gmail.com>
2021-01-05 16:47:10 +0530

    added user input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
 import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+interface = input("interface > ")
+mac_address = input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + mac_address)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + mac_address, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call("ifconfig", shell=True)
