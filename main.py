64f7b6478c3a9ad090f7f9ffd7416f7297294945
kaustubh <saikaustubh10@gmail.com>
2021-01-06 17:48:45 +0530

    handling comman-line arguments

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
 
 interface = input("interface > ")
 mac_address = input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + mac_address)
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + mac_address, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-subprocess.call("ifconfig", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
+subprocess.call(["ifconfig", interface, "up"])
+subprocess.call(["ifconfig"])
