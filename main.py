5036bb204a3f28157526a799c0770f192966187f
kaustubh <saikaustubh10@gmail.com>
2021-01-09 14:07:47 +0530

    completed code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -20,9 +21,24 @@ def change_mac(interface, mac_address):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
     subprocess.call(["ifconfig", interface, "up"])
-    subprocess.call(["ifconfig"])
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode('utf-8')
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + current_mac)
 change_mac(interface= options.interface, mac_address=options.mac_address)
-
+current_mac = get_current_mac(options.interface)
+if current_mac == options.mac_address:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed")
